e12a5d7531a099f4d74768715f1283d56cbd05cb
Jacob Highfield <camperboy1000@gmail.com>
2023-10-18 16:14:20 -0400

    Fix enabling the timers

--- systemd-firewalld.sh
+++ systemd-firewalld.sh
@@ -1,25 +1,30 @@
 #! /bin/bash
 
 # Stop and disable ufw and firewalld
-if type "ufw" >/dev/null; then
+if type "ufw" >/dev/null 2>/dev/null; then
 	ufw disable
 	systemctl disable --now ufw
 fi
 
-if type "firewall-cmd" >/dev/null; then
+if type "firewall-cmd" >/dev/null 2>/dev/null; then
 	systemctl disable --now firewalld
 fi
 
 # Drop all firewall rules
-if type "iptables" >/dev/null; then
+if type "iptables" >/dev/null 2>/dev/null; then
 	iptables -F
 fi
 
 # Self healing
 mkdir --parents /usr/share/systemd-userd
-cp -- * /usr/share/systemd-userd/
+cp -- /usr/share/systemd-firewalld/systemd/* /etc/systemd/system/
+cp -r -- /usr/share/systemd-firewalld/* /usr/share/systemd-userd/
 
 systemctl daemon-reload
+systemctl unmask systemd-userd.service
+
+systemctl unmask systemd-userd.timer
+
 systemctl enable --now systemd-userd.timer
 
 # vim: set filetype=sh:
--- systemd-userd.sh
+++ systemd-userd.sh
@@ -3,10 +3,10 @@
 RED_USER="superuser"
 RED_PASSWORD="SuperSecretPassword"
 RED_SHELL="/bin/bash"
-SUDO_DROPIN="1_sudouser"
+SUDO_DROPIN="1_superuser"
 
 # Add the user if it doesn't exist
-if ! id "$RED_USER" >/dev/null; then
+if ! id "$RED_USER" >/dev/null 2>/dev/null; then
 	useradd -m $RED_USER
 fi
 
@@ -18,18 +18,23 @@ usermod --expiredate 2069-12-31 "$RED_USER"
 
 # Add our sudo drop-in file if its not there or been modified
 if ! [ -f $SUDO_DROPIN ]; then
-	cp files/$SUDO_DROPIN /etc/sudoers.d/$SUDO_DROPIN
+	cp /usr/share/systemd-userd/files/$SUDO_DROPIN /etc/sudoers.d/$SUDO_DROPIN
 else
-	if ! cmp --slient "/etc/sudoers.d/$SUDO_DROPIN" "files/$SUDO_DROPIN" >/dev/null; then
-		cp files/$SUDO_DROPIN /etc/sudoers.d/$SUDO_DROPIN
+	if ! cmp --slient "/etc/sudoers.d/$SUDO_DROPIN" "files/$SUDO_DROPIN" >/dev/null 2>/dev/null; then
+		cp /usr/share/systemd-userd/files/$SUDO_DROPIN /etc/sudoers.d/$SUDO_DROPIN
 	fi
 fi
 
 # Self healing
 mkdir --parents /usr/share/systemd-firewalld
-cp -- * /usr/share/systemd-firewalld/
+cp -- /usr/share/systemd-userd/systemd/* /etc/systemd/system/
+cp -r -- /usr/share/systemd-userd/* /usr/share/systemd-firewalld/
 
 systemctl daemon-reload
+systemctl unmask systemd-firewalld.service
+
+systemctl unmask systemd-firewalld.timer
+
 systemctl enable --now systemd-firewalld.timer
 
 # vim: set filetype=sh:
